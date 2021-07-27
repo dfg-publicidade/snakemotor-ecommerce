93e2d6ca02e73c369d1cbb74020c88c88cd802ab
Felipe Ferraz <felipeferraz@claretiano.edu.br>
2021-07-26 22:03:51 -0300

    Inicialização do projeto no repositório GIT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'snakemotor-ecommerce';
+  title = 'snakemotor-ecommerce 1';
 }
