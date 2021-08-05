1c71652684bc5da1cf034bb990f3112e51510f01
Felipe Ferraz <felipeferraz@claretiano.edu.br>
2021-08-05 17:54:34 -0300

    alteracoes

--- src/app/pages/produto/produto.module.ts
+++ src/app/pages/produto/produto.module.ts
@@ -1,11 +1,12 @@
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { GalleryModule } from 'ngx-doe-gallery';
 import { ProdutoComponentModule } from 'src/app/componentes/produto/produto.module';
 import { ProdutoComponent } from './listagem/produto.component';
 import { ProdutoRoutingModule } from './produto-routing.module';
 import { ProdutoDetailComponent } from './visualizacao/produto-detail.component';
-import { NgSelectModule } from '@ng-select/ng-select';
-import { NgxSliderModule } from '@angular-slider/ngx-slider';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { NgxSliderModule } from '@angular-slider/ngx-slider';
     CommonModule,
     ProdutoComponentModule,
     NgSelectModule,
-    NgxSliderModule
+    NgxSliderModule,
+    GalleryModule
   ],
   providers: []
 })
--- src/app/pages/produto/visualizacao/produto-detail.component.ts
+++ src/app/pages/produto/visualizacao/produto-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GalleryImage } from 'ngx-doe-gallery';
 
 @Component({
   selector: 'app-produto-detail',
@@ -6,12 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produto-detail.component.scss']
 })
 export class ProdutoDetailComponent implements OnInit {
+  images: GalleryImage[];
 
 
   constructor() {
+    this.images = [];
   };
 
 
   ngOnInit(): void {
+    // Set gallery items array
+    this.images = [
+      new GalleryImage('/res/imagens/temporarias/produto-1.jpg', '/res/imagens/temporarias/produto-1.jpg'),
+      new GalleryImage('/res/imagens/temporarias/produto-1.jpg', '/res/imagens/temporarias/produto-1.jpg'),
+      new GalleryImage('/res/imagens/temporarias/produto-1.jpg', '/res/imagens/temporarias/produto-1.jpg'),
+      new GalleryImage('/res/imagens/temporarias/produto-1.jpg', '/res/imagens/temporarias/produto-1.jpg'),
+      new GalleryImage('/res/imagens/temporarias/produto-1.jpg', '/res/imagens/temporarias/produto-1.jpg'),
+      new GalleryImage('/res/imagens/temporarias/produto-1.jpg', '/res/imagens/temporarias/produto-1.jpg'),
+      new GalleryImage('/res/imagens/temporarias/produto-1.jpg', '/res/imagens/temporarias/produto-1.jpg'),
+      new GalleryImage('/res/imagens/temporarias/produto-1.jpg', '/res/imagens/temporarias/produto-1.jpg'),
+      new GalleryImage('/res/imagens/temporarias/produto-1.jpg', '/res/imagens/temporarias/produto-1.jpg'),
+      new GalleryImage('/res/imagens/temporarias/produto-1.jpg', '/res/imagens/temporarias/produto-1.jpg')
+    ];
   }
 }
